c03816a0eec688a455439d128c88e26d4f4ee558
danilo-paixao <danilopaixao@pm.me>
2022-09-01 10:37:03 -0300

    revisão calculadora

--- calculadora.c
+++ calculadora.c
@@ -1,37 +1,36 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-int main(int argc, char *argv[]){
-    int a=0,b=0,c=0;
+int main(int argc, char *argv[]) {
+    int a, b, c;
     char op;
-    printf("Digite o primeiro numero: ");
-    scanf("%d",&a);
-    printf("Digite o segundo numero: ");
-    scanf("%d",&b);
-    printf("Digite a operacao: ");
-    scanf("%c",&op);
-    switch(op){
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+    printf("Digite a operação: ");
+    scanf(" %c", &op);
+    switch (op) {
         case '+':
-            c=a+b;
+            c = a + b;
             break;
         case '-':
-            c=a-b;
+            c = a - b;
             break;
         case '*':
-            c=a*b;
+            c = a * b;
             break;
         case '/':
-            c=a/b;
+            c = a / b;
             break;
         case '^':
-            c=math.pow(a,b);
+            c = pow(a, b);
             break;
         default:
-            printf("Operacao invalida");
-            break;
+            printf("Operação inválida!");
+            return 0;
     }
-    printf("Resultado: %d",c);
     return 0;
 }
