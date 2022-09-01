58fc09702c5303251f7337779792d3a6ad8c9650
danilo-paixao <danilopaixao@pm.me>
2022-09-01 09:48:19 -0300

    Prototipo calculadora

---
+++ calculadora.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main{
+    int a=0,b=0,c=0;
+    char op;
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+    printf("Digite o segundo numero: ");
+    scanf("%d",&b);
+    printf("Digite a operacao: ");
+    scanf("%c",&op);
+    switch(op){
+        case '+':
+            c=a+b;
+            break;
+        case '-':
+            c=a-b;
+            break;
+        case '*':
+            c=a*b;
+            break;
+        case '/':
+            c=a/b;
+            break;
+        case '^':
+            c=pow(a,b);
+            break;
+        default:
+            printf("Operacao invalida");
+            break;
+    }
+    printf("Resultado: %d",c);
+    return 0;
+}
